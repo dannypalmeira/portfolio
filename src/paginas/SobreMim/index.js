6c062ea3bd866e42ec97fffe2446720ba90a797a
Danny Palmeira <99752093+dannypalmeira@users.noreply.github.com>
2025-02-20 17:16:23 +0100

    Update index.js

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -14,7 +14,12 @@ export default function SobreMim() {
         className={styles.fotoSobreMim}
       />
 
-      <p className={styles.paragrafo}></p>
+      <p className={styles.paragrafo}>Ciao! Sono Danielle, brasiliana, designer e sviluppatrice web, ma anche una persona curiosa e sempre in movimento. Ho vissuto e lavorato in diversi posti, tra cui l’Irlanda e l’Italia, e mi piace immergermi nelle culture, adattarmi ai nuovi ambienti e scoprire modi diversi di vivere e lavorare.</p>
+
+ <p className={styles.paragrafo}>Amo ballare, leggere e frequentare corsi per imparare sempre qualcosa di nuovo – che sia design, tecnologia o anche cucina! Parlare di cucina mi ricorda che ogni tanto do una mano a un amico nel settore gastronomico, un’esperienza che unisce la mia passione per il cibo e la voglia di sperimentare.</p>
+        <p className={styles.paragrafo}>Quando posso, scappo nella natura: adoro camminare tra le montagne, lungo la spiaggia o semplicemente in un bel parco. Preferisco le mezze stagioni, quando l’aria è fresca ma non troppo fredda, perfetta per godersi il tempo all’aperto.</p>
+
+ <p className={styles.paragrafo}>La mia compagna di avventure è Mia, la mia gatta, che ama stare seduta accanto a me mentre leggo libri di ogni genere, dai gialli alla sociologia o neuroscienze. È la mia piccola spettatrice anche quando mi immergo nelle mie serie TV coreane preferite! 😊</p>
     </PostModelo>
   );
 }
