c2f2d9c377ae67ff7be8abe299a9486d52e5318f
Danny Palmeira <99752093+dannypalmeira@users.noreply.github.com>
2025-02-20 17:58:15 +0100

    Update index.js

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -38,7 +38,7 @@ export default function Post() {
                 <ReactMarkdown>{post.texto}</ReactMarkdown>
               </div>
 
-              <h2 className={styles.tituloOutrosPosts}>Outros projetos:</h2>
+              <h2 className={styles.tituloOutrosPosts}>Altri progetti:</h2>
               <ul className={styles.postsRecomendados}>{postsRecomendados.map((post) => (
                 <li key={post.id}>
                   <PostCard post={post} />
